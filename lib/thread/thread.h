19dd1783eae82afe2efb6dc76d7b1fa31cae9298
CQNB <1342434213@qq.com>
2022-09-22 15:52:39 +0800

    添加线程类

---
+++ lib/thread/thread.h
@@ -0,0 +1,27 @@
+#ifndef _THREAD_H
+#define _THREAD_H
+
+#include <thread>
+#include "disposable.h"
+
+class Thread : IDisposable{
+
+    public:
+        //启动
+        virtual bool Start();
+        //暂停
+        void Stop();
+        //帧
+        virtual void Update() = 0;
+
+        bool IsRund() const;
+
+        void Dispose() override;
+
+    protected:
+        bool _isRun{false};
+        std::thread _thread;
+
+};
+
+#endif
