cd8fd3ff63c9c123498f0e9ed1514f3da01f21c2
CQNB <1342434213@qq.com>
2022-10-11 01:55:19 +0800

    移除非必须的东西

--- lib/network/networkConnector.h
+++ lib/network/networkConnector.h
@@ -11,6 +11,7 @@ class NetworkConnector : public Network {
         int _port{0};
         ConnectObj *_connectObj{nullptr};
 
+        void HandleDisconnect(Packet *pPacket) override;
 
     public:
         //连接
@@ -29,8 +30,6 @@ class NetworkConnector : public Network {
         virtual ~NetworkConnector() = default;
 
         void Dispose() override;
-    protected:
-        void HandleDisconnect(Packet *pPacket) override;
 };
 
 #endif
